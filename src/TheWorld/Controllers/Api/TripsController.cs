e62d3974844d7815a1c6debf42d9d68595207aa8
rudot <rudotz@gmail.com>
2017-04-07 15:54:12 +0700

    chapter 8-2

--- src/TheWorld/Controllers/Api/TripsController.cs
+++ src/TheWorld/Controllers/Api/TripsController.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
 
 namespace TheWorld.Controllers.Api
 {
     public class TripsController : Controller
     {
-        
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            if (true)
+                return BadRequest("Bad things happen");
+
+            return Ok(new Trip()
+            {
+                Name = "My Trip"
+            });
+        }
     }
 }
